fa517a694d87d7e77b6724ec4a717819479f067a
IhnatVelykoivan <npm>
2024-11-04 20:39:27 +0200

    lesson and homework

---
+++ Lessons/homeworks/hw1/main.js
@@ -0,0 +1,16 @@
+// let a, b , c;
+// a = 'hello';
+// b = 'okten!'
+// c = 'owu.com.ua';
+
+// console.log(a,b,c)
+
+let person = [
+    middleName ='Velykoivan',
+    firstName = 'Ihnat',
+    secondName = 'Oleksandrovich'
+];
+
+console.log(person)
+let a = 100;
+console.log(typeof(a));
