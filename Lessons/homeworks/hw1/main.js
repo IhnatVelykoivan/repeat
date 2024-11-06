c3c7f3b1056a38522168cfb8937b352247812fb7
IhnatVelykoivan <npm>
2024-11-06 11:22:21 +0200

    update

--- Lessons/homeworks/hw1/main.js
+++ Lessons/homeworks/hw1/main.js
@@ -5,12 +5,20 @@
 
 // console.log(a,b,c)
 
-let person = [
-    middleName ='Velykoivan',
-    firstName = 'Ihnat',
-    secondName = 'Oleksandrovich'
-];
-
-console.log(person)
-let a = 100;
-console.log(typeof(a));
+let person = [{
+//     middleName : 'Velykoivan',
+//     firstName : 'Ihnat',
+//     secondName : 'Oleksandrovich'
+// }
+// ];
+//
+// console.log(typeof person,person)
+// // let a = 100;
+// // console.log(typeof(a), a);
+//
+// let middleName = 'Velykoivan';
+// let firstName = 'Ihnat';
+// let secondName = 'Oleksandrovich';
+//
+// let student = `${middleName} ${firstName} ${secondName}`;
+// console.log(student);
